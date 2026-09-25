f95132f030f012344f78d89c397eb47d4c49e857
ayushsingh01845-hue <ayushsingh01845@gmail.com>
2026-09-25 08:48:46 +0530

    Update MainActivity.kt

--- app/src/main/java/com/focuslock/app/MainActivity.kt
+++ app/src/main/java/com/focuslock/app/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
