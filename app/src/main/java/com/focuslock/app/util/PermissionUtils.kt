53eb574528db1e74e8449d889cbaf5d7817a7d8c
ayushsingh01845-hue <ayushsingh01845@gmail.com>
2026-09-26 16:34:51 +0530

    Create PermissionUtils.kt

---
+++ app/src/main/java/com/focuslock/app/util/PermissionUtils.kt
@@ -0,0 +1,15 @@
+package com.focuslock.app.util
+
+import android.app.AppOpsManager
+import android.content.Context
+import android.os.Process
+
+fun hasUsageAccess(context: Context): Boolean {
+    val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+    val mode = appOps.unsafeCheckOpNoThrow(
+        AppOpsManager.OPSTR_GET_USAGE_STATS,
+        Process.myUid(),
+        context.packageName
+    )
+    return mode == AppOpsManager.MODE_ALLOWED
+}
