2de7e47b01c8f86e45cce4799da0c25f9b6f4dfc
ayushsingh01845-hue <ayushsingh01845@gmail.com>
2026-09-26 22:08:48 +0530

    Update Color.kt

--- app/src/main/java/com/focuslock/app/ui/theme/Color.kt
+++ app/src/main/java/com/focuslock/app/ui/theme/Color.kt
@@ -13,3 +13,17 @@ val AccentGreen = Color(0xFF2ECC71)
 val AccentRed = Color(0xFFEF5B5B)
 val TextSecondaryLight = Color(0xFF6B6B78)
 val TextSecondaryDark = Color(0xFFA0A0AD)
+
+// Premium glass-morphism palette
+val GradientTopLight = Color(0xFFEDEAFF)
+val GradientMidLight = Color(0xFFF5F3FF)
+val GradientBottomLight = Color(0xFFFFFFFF)
+
+val GradientTopDark = Color(0xFF201F3D)
+val GradientMidDark = Color(0xFF16162A)
+val GradientBottomDark = Color(0xFF0E0E1A)
+
+val GlassSurfaceLight = Color(0xCCFFFFFF)
+val GlassSurfaceVariantLight = Color(0x99FFFFFF)
+val GlassSurfaceDark = Color(0x33FFFFFF)
+val GlassSurfaceVariantDark = Color(0x1FFFFFFF)
